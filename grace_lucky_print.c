b87b6e975f6d6c525c46b7cae97a7fc19bf13e0d
Gracee023 <gracekevwe@gmail.com>
2023-11-09 14:17:16 +0100

    Print function documentation updated

--- grace_lucky_print.c
+++ grace_lucky_print.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 /**
- * gl_print - Grace's and Lucky's customized print function
- * @word: Massage to be printed
+ * gl_print - Grace's and Lucky's customized print function for this project.
+ * @word: Massage to be printed to the standard output.
  */
 void gl_print(const char *word)
 {
