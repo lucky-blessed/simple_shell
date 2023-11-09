1f24f2ab17734bdf2bbd7b25592419b38ad1e4fa
lucky-blessed <blessednkwor7@gmail.com>
2023-11-09 09:51:03 +0100

    The header file

---
+++ shell.h
@@ -0,0 +1,17 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+
+void gl_prompt(void);
+void gl_print(const char *word);
+void gl_command_exe(const char *command);
+void gl_command_reader(char *command, size_t lent);
+
+#endif /* SHELL_H */
