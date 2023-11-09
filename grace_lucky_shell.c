93a5ee7fb580ac9d7a7e93354a056aad6404d0ed
lucky-blessed <blessednkwor7@gmail.com>
2023-11-09 09:54:07 +0100

    The main customized shell function

---
+++ grace_lucky_shell.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+/**
+ * main - The entry point
+ * Return: 0 (Success).
+ */
+int main(void)
+{
+	char command[120];
+	while (true)
+	{
+		gl_prompt();
+		gl_command_reader(command, sizeof(command));
+		gl_command_exe(command);
+	}
+	return (0);
+}
