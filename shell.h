46f79d4a3186d77ed1d3f27ee31f68e6f4fa456c
lucky-blessed <blessednkwor7@gmail.com>
2023-11-20 17:07:00 +0100

    New header file

--- shell.h
+++ shell.h
@@ -2,15 +2,16 @@
 #define SHELL_H
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
 #include <sys/wait.h>
-#include <sys/types.h>
 
-char *gl_line_reader(void);
-char **gl_line_tok(char *comm);
-size_t gl_strlen(const char *X);
-void gl_cmd_exec(char **args);
+
+void gl_prompt(void);
+void gl_print(const char *word);
+void gl_command_exe(const char *command);
+void gl_command_reader(char *command, size_t lent);
 
 #endif /* SHELL_H */
