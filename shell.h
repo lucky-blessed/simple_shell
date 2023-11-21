320e1356d78ac7a9cbc6c485170e9598aa84a126
lucky-blessed <blessednkwor7@gmail.com>
2023-11-21 20:06:20 +0100

    Updated the header file

--- shell.h
+++ shell.h
@@ -13,5 +13,6 @@ void gl_prompt(void);
 void gl_print(const char *word);
 void gl_command_exe(const char *command);
 void gl_command_reader(char *command, size_t lent);
+void gl_command_args_exe(const char *command);
 
 #endif /* SHELL_H */
