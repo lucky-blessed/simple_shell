dd40be378b8e2f06bf398e0da54b13d182e5c204
lucky-blessed <blessednkwor7@gmail.com>
2023-11-21 20:52:27 +0100

    Modified the main function to handle command lines with arguments

--- grace_lucky_shell.c
+++ grace_lucky_shell.c
@@ -5,17 +5,20 @@
  */
 int main(void)
 {
-	bool pipped = false;
 	char command[120];
 
-	while (true && !pipped)
+	while (true)
 	{
-		if (isatty(STDIN_FILENO) == 0)
-			pipped = true;
-
 		gl_prompt();
 		gl_command_reader(command, sizeof(command));
-		gl_command_exe(command);
+		if (strchr(command, ' ') != NULL)
+		{
+			gl_command_args_exe(command);
+		}
+		else
+		{
+			gl_command_exe(command);
+		}
 	}
 	return (0);
 }
