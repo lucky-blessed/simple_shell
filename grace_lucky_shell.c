4a7a4cc21767d07812f752a25ae5929732e12af9
lucky-blessed <blessednkwor7@gmail.com>
2023-11-24 10:26:09 +0100

    Updated the main function to handle new tasks

--- grace_lucky_shell.c
+++ grace_lucky_shell.c
@@ -5,20 +5,22 @@
  */
 int main(void)
 {
-	char command[120];
+	char command[BUFFER_SIZE];
 
 	while (true)
 	{
 		gl_prompt();
 		gl_command_reader(command, sizeof(command));
+		gl_command_exe(command);
 		if (strchr(command, ' ') != NULL)
-		{
-			gl_command_args_exe(command);
-		}
-		else
 		{
 			gl_command_exe(command);
 		}
+		else
+		{
+			gl_path_handler(command);
+		}
+		gl_path_handler(command);
 	}
 	return (0);
 }
